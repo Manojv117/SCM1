349982e17e84c043c7456404859e2033f25b2add
manoj.v1@s.amity.edu <manoj.v1@s.amity.edu>
2025-03-04 11:06:20 +0530

    code of number is odd or evn

---
+++ evnorodd.c
@@ -0,0 +1,11 @@
+// finding out number is evn or odd
+
+#include<stdio.h>
+ int main(){
+ int num;
+  printf(" ENter the number");
+  scanf("%d,&num);
+ if(num%2==0)
+  printf("number is evn");
+ retun 0;
+}
